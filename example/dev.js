65947b743909f9cf192276083c89aeb88da2f962
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-07-17 23:20:19 +0200

    Experiment with wrapper to support browser and node usage

---
+++ example/dev.js
@@ -0,0 +1,5 @@
+const main = require('../src/index');
+
+console.log(main.foo);
+main.setStr("test123");
+console.log(main.getStr());
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-exports.testMsg = function() {
-    console.log("Test123");
-}
+// wrapper adapted from https://github.com/steverydz/build-url/blob/master/src/build-url.js: to support usage with browser, ES6 modules and node
+;(function () {
+    let str = "";
+
+    const main = {
+        foo: "bar",
+        setStr: s => {
+            str = s;
+        },
+        getStr: () => {
+            return str;
+        }
+    };
+
+    if (typeof(exports) !== 'undefined') {
+        if (typeof(module) !== 'undefined' && module.exports) {
+            exports = module.exports = main;
+        }
+        exports.main = main;
+    } else {
+        this.main = main;
+    }
+}).call(this);
