1a4f46f4d995fd4efa392cfda09d4d57c70cbbda
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-07-22 18:43:12 +0200

    Archive-commit code snippet how to use Yasqe in Yasgui for parsing
    I will probably use a dedicated SPARQL parsing library instead of Yasgui that is build on the general code parsing library CodeMirror. But I wanted to have this archived in the commit history - this would be a start how to make this work with Yasgui.

--- src/sparql-editor.js
+++ src/sparql-editor.js
@@ -11,6 +11,12 @@ const initSparqlEditor = editorDiv => {
     }
     tab = yasgui.addTab(true, {});
     tab.setQuery("SELECT * WHERE {\n ?sub ?pred ?obj .\n}");
+
+    let yasqe = yasgui.getTab().getYasqe();
+    // can't be found via IDE in Yasgui lib, but is there via CodeMirror dependency
+    console.log(yasqe.getLineTokens(0)); // via https://discuss.codemirror.net/t/how-can-i-traverse-through-tokens/81/2
+    let linesCount = yasqe.getDoc().children[0].lines.length;
+    // ...
 };
 
 export { initSparqlEditor }
