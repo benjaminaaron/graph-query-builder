05a2cb5d880548ae6760a3f2103b5da901278c5c
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-07-23 23:14:48 +0200

    Use SPARQL.js to parse the query in Yasgui
    Recommended by @LaurensRietveld because it supports a parse tree that I can work with. Yasgui uses a streamed tokenizer.

--- src/model.js
+++ src/model.js
@@ -1,13 +1,32 @@
 import { onValidEditorChange } from './sparql-editor'
+const SparqlParser = require('sparqljs').Parser;
+const parser = new SparqlParser();
 
 let queryModel;
 let graphModel;
 
 const initModel = () => {
     onValidEditorChange(queryStr => {
-        console.log(queryStr);
+        buildGraphFromQuery(parser.parse(queryStr));
     });
     // onGraphChange(graph => {});
+};
+
+const buildGraphFromQuery = queryModel => {
+    let queryType = queryModel.queryType;
+    let prefixes = queryModel.prefixes;
+    let variables = queryModel.variables.map(varObj => varObj.value);
+    queryModel.where[0].triples.forEach(triple => {
+        let sub = triple.subject;
+        let pred = triple.predicate;
+        let obj = triple.object;
+
+        // TODO
+    });
+};
+
+const buildQueryFromGraph = graphModel => {
+   // TODO
 };
 
 export { initModel }
