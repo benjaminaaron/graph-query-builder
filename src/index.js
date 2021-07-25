5494f05aa7903e6d42d4c2d4f2a592b5747b2411
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-07-25 22:46:28 +0200

    Outcomment graphOutput for now

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { initGraphBuilder } from './graph-builder';
 import { initSparqlEditor } from './sparql-editor';
-import { initGraphOutput } from './graph-output';
+// import { initGraphOutput } from './graph-output';
 import { initModel } from "./model";
 
 window.init = config => {
     initGraphBuilder(config.graphBuilder);
     initSparqlEditor(config.sparqlEditor);
-    initGraphOutput(config.graphOutput);
+    // initGraphOutput(config.graphOutput);
     initModel();
 };
