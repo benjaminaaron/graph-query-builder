611690df22b268df738e0f3f15cb30edc5a7493a
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-09-08 21:46:22 +0200

    Archive-commit: remove unused parseSentences() method

--- src/language-interpreter.js
+++ src/language-interpreter.js
@@ -23,32 +23,11 @@ CodeMirror.defineSimpleMode("sparqlTermTypes", {
 const initLanguageInterpreter = config => {
     config.div.style.border = "1px solid silver";
     editor = CodeMirror(config.div, {
-        value: "some text in line one.\none some text in the second line.",
+        value: "",
         mode:  "sparqlTermTypes",
         readOnly: true,
         lineWrapping: true
     });
-};
-
-const parseSentences = () => {
-    let tokens = [];
-    for (let i = 0; i < editor.getDoc().lineCount(); i ++) {
-        tokens = [...tokens, ...editor.getLineTokens(i).filter(token => token.string.trim())];
-    }
-    let sentences = [];
-    let oneSentence = [];
-    tokens.forEach(token => {
-        if (token.string === ".") {
-            sentences.push(oneSentence);
-            oneSentence = [];
-        } else {
-            oneSentence.push(token.string);
-        }
-    });
-    if (oneSentence.length > 0) { // no dot at all or dangling words in the end without closing with a dot
-        sentences.push(oneSentence);
-    }
-    return sentences;
 };
 
 const setEditorValue = (value, _keywords = { NamedNode: [], Variable: [], Literal: [] }) => {
@@ -57,7 +36,6 @@ const setEditorValue = (value, _keywords = { NamedNode: [], Variable: [], Litera
 };
 
 const onEditorChange = onChange => {
-    // editor.on("change", () => onChange(parseSentences()));
     editor.on("keyup", (obj, event) => {
         if (event.key !== "Enter") {
             alert("Translating the natural language domain to the SPARQL and graph domain is not supported yet... or ever. Quite tough to get this right I imagine :)")
