64bb0a3d8444f818f8f4a65054e1f6c70b989a11
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-09-08 22:09:23 +0200

    Move query result table logic into own js file

--- src/model.js
+++ src/model.js
@@ -1,14 +1,14 @@
 import { onValidSparqlChange, setSparqlQuery, getQuery } from './panels/sparql-editor'
 import { setGraphBuilderData, onValidGraphChange } from './panels/graph-builder';
 import { onEditorChange, updateLanguageEditor } from "./panels/language-interpreter";
-import { querySparqlEndpoint, fetchAllTriplesFromEndpoint, extractTriplesFromQuery, buildShortFormIfPrefixExists } from "./utils";
+import { querySparqlEndpoint, fetchAllTriplesFromEndpoint, extractTriplesFromQuery } from "./utils";
 import { setGraphOutputData } from "./panels/graph-output";
+import { buildTable } from "./panels/results-table";
 
 const parser = new require('sparqljs').Parser();
 const generator = new require('sparqljs').Generator();
 let currentSparqlModel;
 let outputElements;
-let selectedRow = null;
 
 const Domain = {
     SPARQL: 1, GRAPH: 2, LANGUAGE: 3
@@ -43,50 +43,12 @@ const initModel = _outputElements => {
 
 const submitSparqlQuery = () => {
     outputElements.outputWrapperDiv.style.display = 'flex';
-    let query = getQuery();
-    let prefixes = currentSparqlModel.prefixes
-
+    let prefixes = currentSparqlModel.prefixes;
     fetchAllTriplesFromEndpoint(prefixes, graphData => {
         setGraphOutputData(graphData);
-        querySparqlEndpoint(query, (variables, rows) => {
+        querySparqlEndpoint(getQuery(), (variables, rows) => {
             console.log("query result:", variables, rows);
-            let parentEl = outputElements.queryResultsDiv;
-            while (parentEl.firstChild) parentEl.removeChild(parentEl.lastChild);
-            let table = document.createElement('table');
-            table.setAttribute("id", "queryResultsTable");
-            let tr = document.createElement('tr');
-            variables.forEach(col => {
-                let th = document.createElement('th');
-                let text = document.createTextNode("?" + col.value);
-                th.appendChild(text);
-                tr.appendChild(th);
-            });
-            table.appendChild(tr);
-            rows.forEach(row => {
-                tr = document.createElement('tr');
-                row.tr = tr;
-                variables.forEach(col => {
-                    let cell = row[col.value];
-                    let td = document.createElement('td');
-                    td.classList.add(cell.termType);
-                    let text = document.createTextNode(buildShortFormIfPrefixExists(prefixes, cell.value));
-                    td.appendChild(text);
-                    tr.appendChild(td);
-                });
-                tr.addEventListener("click", () => {
-                    selectedRow && selectedRow.tr.classList.remove("selectedRow");
-                    if (row === selectedRow) {
-                        selectedRow = null;
-                    } else {
-                        selectedRow = row;
-                        row.tr.classList.add("selectedRow");
-                    }
-
-                    // TODO
-                });
-                table.appendChild(tr);
-            });
-            parentEl.appendChild(table);
+            buildTable(variables, rows, prefixes);
         }).then();
     });
 };
