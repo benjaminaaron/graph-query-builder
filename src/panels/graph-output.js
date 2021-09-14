8425de5ec5fb9c2d78c167ff7f84acce87351725
Benjamin Aaron Degenhart <b.degenhart.work@gmail.com>
2021-09-14 23:04:36 +0200

    Filter triples (edges plus their attached nodes) to highlight

--- src/panels/graph-output.js
+++ src/panels/graph-output.js
@@ -13,7 +13,27 @@ const setGraphOutputData = graphData => {
 };
 
 const highlightGraphOutputSubset = graphData => {
+    let triplesToHighlight = edges.filter(edge => containsSameTriple(edge, graphData));
+
     // TODO
+};
+
+const containsSameTriple = (thisEdge, otherGraphData) => {
+    // can this be achieved in a better way?
+    let thisEdgeValue = thisEdge.value;
+    let thisSourceValue = nodes[thisEdge.source.id].value;
+    let thisTargetValue = nodes[thisEdge.target.id].value;
+    let otherNodes = otherGraphData.nodes;
+    let otherEdges = otherGraphData.edges;
+    for (let i = 0; i < otherEdges.length; i++) {
+        let otherEdgeValue = otherEdges[i].value;
+        let otherSourceValue = otherNodes[otherEdges[i].source].value;
+        let otherTargetValue = otherNodes[otherEdges[i].target].value;
+        if (thisEdgeValue === otherEdgeValue && thisSourceValue === otherSourceValue && thisTargetValue === otherTargetValue) {
+            return true;
+        }
+    }
+    return false;
 };
 
 const setLabelAndTooltip = (prefixes, nodeOrEdge) => {
